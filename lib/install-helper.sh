a4ee60cfef9b8b726d8f4e9c20d9a2981750717e
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2020-09-03 21:40:02 +0100

    skip prexec in zsh

--- lib/install-helper.sh
+++ lib/install-helper.sh
@@ -17,8 +17,10 @@ function set_environment_exports {
         fi
     fi
 
+    [[ $2 = "bash" ]] && return 1
+
     # Check if we have a .zshrc regardless of the os .. and copy that to the zsh source file
-    if [[ -f "$HOME/.zshrc" && $2 -eq "bash" ]]; then
+    if [[ -f "$HOME/.zshrc" ]]; then
         if ! grep -q "${1}" "${HOME}/.zshrc" ; then
           printf "%s\n" "Noticed that you have Zsh installed (there might be some compatibility issues exporting there as well)"
           read -p "Are you sure you want to proceed exporting in your .zshrc ? [Y/N] " -n 1;
